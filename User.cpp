d102482bd07d6d6b22ee292fe37c7cb60756073d
howorang <howorang@gmail.com>
2017-05-08 19:23:32 +0200

    User implementation

---
+++ User.cpp
@@ -0,0 +1,29 @@
+//
+// Created by howorang on 08.05.17.
+//
+
+#include "User.h"
+#include "Ice/Ice.h"
+
+UserI::UserI(std::string name) {
+    this->name = name;
+}
+
+::std::string UserI::getName(const ::Ice::Current & current) {
+    return name;
+}
+
+void UserI::receiveText(const ::std::string & message, const ::Chat::UserPrx & sender, const ::Chat::GroupServerPrx & groupServerPrx,
+                        const ::Ice::Current & current) {
+    std::string senderUserName = sender -> getName();
+    std::string groupName = groupServerPrx -> Name();
+    std::cout << groupName << " " << senderUserName << " " << message << std::endl;
+}
+
+void UserI::receivePrivateText(const ::std::string & message, const ::Chat::UserPrx & sender,
+                               const ::Ice::Current & current) {
+    std::string senderUserName = sender -> getName();
+    std::cout << "PRIV: " << senderUserName << " " << message << std::endl;
+}
+
+
--- User.h
+++ User.h
@@ -6,15 +6,24 @@
 #define ICECHAT_USER_H
 
 #include "chat.h"
+#include <string>
 
 using namespace Chat;
 
 class UserI : public virtual User {
+
+public:
+    UserI(std::string name);
+
     virtual ::std::string getName(const ::Ice::Current& = ::Ice::Current());
 
     virtual void receiveText(const ::std::string&, const ::Chat::UserPrx&, const ::Chat::GroupServerPrx&, const ::Ice::Current& = ::Ice::Current());
 
     virtual void receivePrivateText(const ::std::string&, const ::Chat::UserPrx&, const ::Ice::Current& = ::Ice::Current());
+
+private:
+
+    std::string name;
 };
 
 #endif //ICECHAT_USER_H
